af93369a94ed0f037e088163e8ebd495b84c4b58
hvanlam <49455678+hvanlam@users.noreply.github.com>
2020-04-17 17:19:23 -0400

    Create node.js

---
+++ node.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let app = express();
+let port = 5000;
+
+app.get('/', (req,res) => {
+  res.sendFile('./index.html', {root: _dirname});
+});
+
+app.listen(port, () => console.log('listening on port: ' + port);
