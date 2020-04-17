a982d194c5336c24707d9c91c3f935a6af11b08f
hvanlam <49455678+hvanlam@users.noreply.github.com>
2020-04-17 19:19:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get('/', (req,res) => {
   res.sendFile('./index.html', {root: _dirname});
 });
 
-app.listen(port, () => console.log('listening on port: ' + port);
+app.listen(port, () => console.log('listening on port: ' + port));
