c53864d53e34d47d48ec5d0840c7c0ae182157f5
hvanlam <49455678+hvanlam@users.noreply.github.com>
2020-04-17 20:20:04 -0400

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let app = express();
-let port = 5000;
-
-app.get('/', (req,res) => {
-  res.sendFile('./index.html', {root: _dirname});
-});
-
-app.listen(port, () => console.log('listening on port: ' + port));
