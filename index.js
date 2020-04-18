1b7fe3fdfeba4b13bd13c1bbb9a7fc012441b5ec
hvanlam <49455678+hvanlam@users.noreply.github.com>
2020-04-17 20:28:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
-app.get('/', (req,res) => res.sendFile('./index.html', {root: __dirname}));
+//app.get('/', (req,res) => res.sendFile('./index.html', {root: __dirname}));
+app.get('/', (req,res) => res.send('hello world'));
 
 app.listen(port, () => console.log('Example log listening at ' + port));
